ccef6e6acc4ee84ea4cfe5970e2d55f38b81f2ab
Estrella Razo <estrella.rgz17@gmail.com>
2024-05-22 22:09:02 -0600

    Se agregaron archivos .php

--- backend/db/Database.php
+++ backend/db/Database.php
@@ -3,7 +3,15 @@
         private $con;
 
         public function --construct(){
-            this->conn = new mysqli('localhost:8889', 'root', 'root', 'auto-aventura');
+            this->con = new mysqli('localhost:8889', 'root', 'root', 'auto-aventura');
+            
+            if($this->con->connect_error){
+                die('Error en la conexión a la db' . $this->vcon->connect_error);
+            }
+
+            public function getConnection(){
+                return $this->con;
+            }
         }
     }
 ?>
